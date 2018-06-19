9c154d3e7a6656b819223131c1fd8a9d09929429
Phojie <rengelphojie@yahoo.com>
2018-06-19 22:53:06 +0200

    test

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable implements JWTSubject
 	 * @var array
 	 */
 	protected $fillable = [
-		'id','name', 'email', 'password',
+		'name', 'email', 'password',
 	];
 
 	/**
