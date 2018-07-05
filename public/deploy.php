edf5c8781125dea364aa9dc2099ea9868df0265c
Phojie <rengelphojie@yahoo.com>
2018-07-05 08:03:28 +0200

    test

--- public/deploy.php
+++ public/deploy.php
@@ -1,10 +1,10 @@
 <?php
 
-$repo_dir = '/srv/users/serverpilot/apps/ckcm-network';
+// $repo_dir = '/srv/users/serverpilot/apps/ckcm-network';
 
-if (isset($_GET['secret']) && $_GET['secret'] == 'jiengpinas')
-   exec('cd' . $repo_dir);
-   exec('git pull origin master');
-   exec('composer install');
-   exec('php artisan migrate');
+// if (isset($_GET['secret']) && $_GET['secret'] == 'jiengpinas')
+   // exec('cd' . $repo_dir);
+   // exec('git pull origin master');
+   // exec('composer install');
+   // exec('php artisan migrate');
    // exec('php artisan jwt:secret');
