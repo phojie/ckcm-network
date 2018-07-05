32bba60577ccc20454067a3a80caf0926f86d253
Phojie <rengelphojie@yahoo.com>
2018-07-05 04:46:24 +0200

    test

--- public/deploy.php
+++ public/deploy.php
@@ -2,7 +2,7 @@
 
 $repo_dir = '/srv/users/serverpilot/apps/ckcm-network';
 
-if($isset($_GET['secret']) && $_GET['secret'] == 'jiengpinas')
+if (isset($_GET['secret']) && $_GET['secret'] == 'jiengpinas')
    exec('cd' . $repo_dir);
    exec('git pull origin master');
    exec('composer install');
